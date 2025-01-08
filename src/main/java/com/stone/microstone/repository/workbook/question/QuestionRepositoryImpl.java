b4b08d2796ae8d2f3c25d1ef1c7d6c3f0293f98f
beginnerones <huns0905@naver.com>
2025-01-08 23:00:36 +0900

    [setting] question setting and fix

--- src/main/java/com/stone/microstone/repository/workbook/question/QuestionRepositoryImpl.java
+++ src/main/java/com/stone/microstone/repository/workbook/question/QuestionRepositoryImpl.java
@@ -1,2 +1,55 @@
-package com.stone.microstone.repository.workbook.question;public class QuestionRepositoryImpl {
+package com.stone.microstone.repository.workbook.question;
+
+import com.stone.microstone.domain.entitiy.Question;
+import com.stone.microstone.domain.entitiy.WorkBook;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class QuestionRepositoryImpl implements QuestionRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public QuestionRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+
+    @Override
+    public Question save(Question question) {
+        if(question.getPr_id() == 0){
+            em.persist(question);
+        }else{
+            em.merge(question);
+        }
+        return question;
+    }
+
+    @Override
+    public Optional<Question> findById(int id) {
+        return em.createQuery("SELECT q FROM Question q WHERE q.pr_id=:id",Question.class)
+                .setParameter("id",id)
+                .getResultList().stream().findFirst();
+    }
+
+    @Override
+    public List<Question> findAll(WorkBook workBook) {
+        return em.createQuery("SELECT q FROM Question q WHERE q.workBook=:workBook",Question.class)
+                .setParameter("workBook",workBook)
+                .getResultList();
+    }
+
+    @Override
+    public Optional<Question> findLastQuestion(WorkBook workBook) {
+        return em.createQuery("SELECT MIN(q.pr_id) FROM Question q WHERE q.workBook=:workBook",Question.class)
+                .setParameter("workBook",workBook)
+                .getResultList().stream().findFirst();
+    }
+
+
 }
