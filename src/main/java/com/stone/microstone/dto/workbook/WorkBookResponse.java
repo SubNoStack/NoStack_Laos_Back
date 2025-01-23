acda0d9854fe2e3905aeb33e0c85ff226e11d0f2
bindoong01 <mailsky9687@gmail.com>
2025-01-23 21:31:02 +0900

    문제집 응답 데이터 날짜 타입 수정

--- src/main/java/com/stone/microstone/dto/workbook/WorkBookResponse.java
+++ src/main/java/com/stone/microstone/dto/workbook/WorkBookResponse.java
@@ -1,5 +1,6 @@
 package com.stone.microstone.dto.workbook;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ public class WorkBookResponse {
     @Schema(description = "문제집 제목",example = "문제집2")
     private String wb_title;
     @Schema(description = "문제집 생성 시간",example="2025-01-14")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate wb_create;
     @Schema(description = "문제집 전체 내용.",example = "문제집 내용")
     private String wb_content;
